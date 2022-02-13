85749ce122a6e34a5e667f3a1c7100535d43ab08
Kjoedicker <kjoedicker@gmail.com>
2022-02-13 13:16:57 -0500

    test(make_commits): add shell script to generate commits on the fly for testing:

---
+++ test/make_commits.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+touch a.txt
+git add a.txt
+git commit -m "feat(a.txt): here a.txt"
+
+touch b.txt
+git add b.txt
+git commit -m "feat(b.txt): here b.txt"
+
+touch c.txt
+git add c.txt
+git commit -m "feat(c.txt): here c.txt"
+
+touch d.txt
+git add d.txt
+git commit -m "feat(d.txt): here d.txt"
+
+touch e.txt
+git add e.txt
+git commit -m "feat(e.txt): here e.txt"
+
+
+
+
+
